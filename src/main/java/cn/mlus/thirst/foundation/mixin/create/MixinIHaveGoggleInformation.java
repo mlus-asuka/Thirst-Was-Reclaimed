058fd57f5324ed6650b5be53162b791d51172d58
mlus <1319237806@qq.com>
2026-08-08 18:24:10 +0800

    #36

--- src/main/java/cn/mlus/thirst/foundation/mixin/create/MixinIHaveGoggleInformation.java
+++ src/main/java/cn/mlus/thirst/foundation/mixin/create/MixinIHaveGoggleInformation.java
@@ -1,9 +1,9 @@
 package cn.mlus.thirst.foundation.mixin.create;
 
 
+import cn.mlus.thirst.content.purity.WaterPurity;
 import com.simibubi.create.api.equipment.goggles.IHaveGoggleInformation;
 import com.simibubi.create.foundation.utility.CreateLang;
-import cn.mlus.thirst.content.purity.WaterPurity;
 import net.createmod.catnip.lang.LangBuilder;
 import net.minecraft.ChatFormatting;
 import net.minecraft.network.chat.Component;
@@ -11,7 +11,9 @@ import net.minecraft.world.level.material.Fluids;
 import net.neoforged.neoforge.fluids.FluidStack;
 import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.List;
 
@@ -21,14 +23,19 @@ public interface MixinIHaveGoggleInformation {
      * @author mlus
      * @reason add purity information
      */
-    @Overwrite
-    default boolean containedFluidTooltip(List<Component> tooltip, boolean isPlayerSneaking,
-                                          IFluidHandler handler) {
-        if (handler == null)
-            return false;
+    @Inject(method = "containedFluidTooltip", at = @At("HEAD"), cancellable = true)
+    private void containedFluidTooltip(List<Component> tooltip, boolean isPlayerSneaking, IFluidHandler handler, CallbackInfoReturnable<Boolean> cir) {
+        if (handler == null){
+            cir.setReturnValue(false);
+            return;
+        }
 
-        if (handler.getTanks() == 0)
-            return false;
+
+        if (handler.getTanks() == 0){
+            cir.setReturnValue(false);
+            return;
+        }
+
 
         LangBuilder mb = CreateLang.translate("generic.unit.millibuckets");
         CreateLang.translate("gui.goggles.fluid_container")
@@ -72,11 +79,15 @@ public interface MixinIHaveGoggleInformation {
         if (handler.getTanks() > 1) {
             if (isEmpty)
                 tooltip.removeLast();
-            return true;
+            cir.setReturnValue(true);
+            return;
         }
 
-        if (!isEmpty)
-            return true;
+        if (!isEmpty){
+            cir.setReturnValue(true);
+            return;
+        }
+
 
         CreateLang.translate("gui.goggles.fluid_container.capacity")
                 .add(CreateLang.number(handler.getTankCapacity(0))
@@ -85,7 +96,7 @@ public interface MixinIHaveGoggleInformation {
                 .style(ChatFormatting.GRAY)
                 .forGoggles(tooltip, 1);
 
-        return true;
+        cir.setReturnValue(true);
     }
 
     default ChatFormatting getPurityColor(int purity){
