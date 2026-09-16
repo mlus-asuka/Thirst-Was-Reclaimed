3a4326893b726010bbd7580c557de3d8447fc8a9
mlus <1319237806@qq.com>
2026-09-16 16:01:33 +0800

    fix #40

--- src/main/java/cn/mlus/thirst/foundation/mixin/MixinCauldronBlock.java
+++ src/main/java/cn/mlus/thirst/foundation/mixin/MixinCauldronBlock.java
@@ -3,10 +3,8 @@ package cn.mlus.thirst.foundation.mixin;
 import cn.mlus.thirst.content.purity.WaterPurity;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.LayeredCauldronBlock;
+import net.minecraft.world.level.block.CauldronBlock;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.level.block.state.StateDefinition;
 import net.minecraft.world.level.material.Fluid;
 import net.minecraft.world.level.material.Fluids;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,22 +12,13 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-
-@Mixin(LayeredCauldronBlock.class)
-public abstract class MixinLayeredCauldronBlock
+@Mixin(CauldronBlock.class)
+public abstract class MixinCauldronBlock
 {
-    @Inject(method = "createBlockStateDefinition", at = @At("HEAD"))
-    protected void addPurityBlockState(StateDefinition.Builder<Block, BlockState> p_153549_, CallbackInfo ci)
-    {
-        p_153549_.add(WaterPurity.BLOCK_PURITY);
-    }
-
     @Inject(method = "receiveStalactiteDrip", at = @At("TAIL"))
     private void markDripstoneWaterDirty(BlockState state, Level level, BlockPos pos, Fluid fluid, CallbackInfo ci)
     {
-        if (WaterPurity.isEnabled()
-                && fluid == Fluids.WATER
-                && state.getValue(LayeredCauldronBlock.LEVEL) < LayeredCauldronBlock.MAX_FILL_LEVEL)
+        if (WaterPurity.isEnabled() && fluid == Fluids.WATER)
         {
             BlockState filledState = level.getBlockState(pos);
             level.setBlockAndUpdate(pos, filledState.setValue(WaterPurity.BLOCK_PURITY, WaterPurity.MIN_PURITY + 1));
